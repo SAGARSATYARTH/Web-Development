44645b1a0ea4f092f8bfc4e8ba2c2511291bf637
SAGAR SATYARTH <sagarsatyarthh@gmail.com>
2026-05-11 13:07:22 +0530

    initial commit

---
+++ JavaScript/form1.js
@@ -0,0 +1,14 @@
+let nm=document.querySelector("#name");
+let form=document.querySelector("form");
+let hide=document.querySelector("#hide");
+
+form.addEventListener("submit",function(dets){
+    dets.preventDefault();
+
+    if(nm.value.length<=2){
+        hide.style.display="initial";
+    }
+    else{
+        hide.style.display="none";
+    }
+})
